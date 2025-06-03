b95b4827e787a31f92e0d10200952e9e042e531d
jackhung32768 <einsteinhung@gmail.com>
2025-06-04 00:02:15 +0800

    added the temperature converter

--- main.go
+++ main.go
@@ -50,8 +50,33 @@ func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Python 回傳結果：%s", string(body))
 }
 
+func temp_handler(w http.ResponseWriter, r *http.Request) {
+    celsius := r.URL.Query().Get("celsius")
+    fahrenheit := r.URL.Query().Get("fahrenheit")
+
+    if celsius == "" && fahrenheit == "" {
+        http.ServeFile(w, r, "index_temp.html")
+        return
+    }
+
+    url := fmt.Sprintf(
+        "https://python-api-5rg4.onrender.com/convert_between_celsius_and_fahrenheit?celsius=%s&fahrenheit=%s",
+        celsius, fahrenheit)
+
+    resp, err := http.Get(url)
+    if err != nil {
+        http.Error(w, "無法連接 Python API", 500)
+        return
+    }
+    defer resp.Body.Close()
+    body, _ := ioutil.ReadAll(resp.Body)
+
+    fmt.Fprintf(w, "Python 回傳結果：%s", string(body))
+}
+
 func main() {
     http.HandleFunc("/", handler)
+    http.HandleFunc("/temperature_convert", temp_handler)
     fmt.Println("Go 伺服器啟動：localhost:8080")
 //    log.Fatal(http.ListenAndServe(":8080", nil))
     port := os.Getenv("PORT")
